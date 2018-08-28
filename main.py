f1501a50dcfb702ca39f93c514154beedb7c9210
Frioa <916378508@qq.com>
2018-08-28 18:23:53 +0800

    爬取 知乎搜索页面

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy","crawl","maoyan"])
+execute(["scrapy","crawl","zhihu"])
+# execute(["scrapy","crawl","jobbole"])
+# execute(["scrapy","crawl","maoyan"])
