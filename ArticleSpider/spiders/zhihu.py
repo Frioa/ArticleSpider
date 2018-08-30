4ea2c44e4dda7d7b1f7e88f7b071f79195b3ea8b
Frioa <916378508@qq.com>
2018-08-30 19:36:19 +0800

    json 页面爬取

--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import re
 import scrapy
+import json
+import datetime
 from scrapy.conf import settings
 
 from urllib import parse
@@ -15,6 +17,9 @@ class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     #allowed_domains = ['www.zhihu.com']
     start_urls = ['https://www.zhihu.com/search?type=content&q=python']
+
+    # question的第一页answer的请求url
+    start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit={1}&offset={2}"
     header = {
         'cookie' : 'd_c0="AKAignGUpA2PTtmB2rpdIGxSlOCFClmojFw=|1527163066";'
               ' _zap=28412dda-f7ac-4290-863f-b8c3d662bfbf; q_c1=2bc6ee9ec0294d69830ad3265df08a11|1533820485000|1527163066000;'
@@ -89,4 +94,32 @@ class ZhihuSpider(scrapy.Spider):
             item_loader.add_css("topics", ".zm-tag-editor-labels a::text")
 
             question_item = item_loader.load_item()
+
+        yield scrapy.Request(self.start_answer_url.format(request_id, 20, 0), headers=self.header, callback=self.parse_answer)
         yield question_item
+
+    def parse_answer(self, response):
+        # 处理question的answer
+        ans_json = json.loads(response.text)
+
+        is_end = ans_json["paging"]["is_end"]
+        totals_answer = ans_json["paging"]["totals"]
+        next_url = ans_json["paging"]["next"]
+
+        # 提取answer具体字段
+        for answer in ans_json["data"]:
+            answer_item = ZhihuAnswerItem()
+            answer_item["zhihu_id"] = answer["id"]
+            answer_item["url"] = answer["url"]
+            answer_item["question_id"] = answer["question"]["id"]
+            answer_item["author_id"] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item["content"] = answer["content"] if "content" in answer else None
+            answer_item["parise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
+            answer_item["create_time"] = answer["created_time"]
+            answer_item["update_time"] = answer["updated_time"]
+            answer_item["crawl_time"] = datetime.datetime.now()
+            yield answer_item
+
+        if not is_end:
+         yield scrapy.Request(next_url, headers=self.header, callback=self.parse_answer)
--- ArticleSpider/utils/zhuhu_login_requests.py
+++ ArticleSpider/utils/zhuhu_login_requests.py
@@ -130,7 +130,7 @@ head = {
     'HOST': 'www.zhihu.com',
     'User-Agent': agent,
 }
-r = requests.get('https://www.zhihu.com', headers=head)
+r = requests.get('https://www.zhihu.com/api/v4/questions/20702054/answers?include=data[*].is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata[*].mark_infos[*].url%3Bdata[*].author.follower_count%2Cbadge[%3F(type%3Dbest_answerer)].topics&limit=15&offset=20', headers=head)
 print(r.text)
 
 # is_login()
